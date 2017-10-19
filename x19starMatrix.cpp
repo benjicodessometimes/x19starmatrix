b54f153b4a0a255120a9d106a262f06b6116dfb1
benjicodessometimes <31226169+benjicodessometimes@users.noreply.github.com>
2017-10-18 17:22:48 -0700

    Add files via upload

---
+++ x19starMatrix.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+vector <vector <int> > stars();
+cout << "enter two values"<< endl;
+int val1,val2;
+  if(val1>0 && val2>0)
+      {
+         for(int i =0; i < val1;i++)
+         {
+           for(int j =0;j<val2;j++)
+           {
+             cout<< "*";
+           }
+           cout<<" " << endl;
+        }
+      }
+      else if(i==0 || j == 0)
+      {
+      cout<< "no stars" << endl;
+      }
+ }
